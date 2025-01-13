37565910552cb9b2594e4080fe9a0137f2c151cc
unknown <met98zoo@gmail.com>
2025-01-13 19:17:13 +0000

    module backoffice

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
   },
+  // {
+  //   path: 'backoffice', component: BackofficeLayoutComponent, canActivate: [AuthGuard], data: { roles: ['ROLE_ADMIN'] },
+  //   loadChildren: () => import('./modules/backoffice/backoffice.module').then(m => m.BackofficeModule),
+
+  // },
 
 ];
 
